840f87e8f67e2bcecc5a784f998c334caef3d34e
Björn Engel <bjoern@bjoern-engel.de>
2015-12-24 00:22:03 +0100

    Tutorial step 3, but without theme

--- dropwizard-vaadin-meinhotel/src/main/java/org/bonn/se/meinhotelapp/ui/MyUI.java
+++ dropwizard-vaadin-meinhotel/src/main/java/org/bonn/se/meinhotelapp/ui/MyUI.java
@@ -1,5 +1,6 @@
 package org.bonn.se.meinhotelapp.ui;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
@@ -11,12 +12,16 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.data.util.BeanContainer;
+import com.vaadin.data.util.BeanItem;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
@@ -36,17 +41,13 @@ public class MyUI extends UI {
 	public static class Servlet extends VaadinServlet {
 	}
 
-	private int clickCounter = 0;
-
-	private Label clickCounterLabel;
+	private Hotel hotelSelektiert = null;
+	private int anzahlSuchen = 0;
 
 	@Override
 	protected void init(VaadinRequest request) {
 		final VerticalLayout layout = new VerticalLayout();
 		setContent(layout);
-
-		clickCounterLabel = new Label();
-		layout.addComponent(clickCounterLabel);
 
 		final HorizontalLayout horizintallayout = new HorizontalLayout();
 		Button button = new Button("Suche", FontAwesome.SEARCH);
@@ -74,9 +75,13 @@ public class MyUI extends UI {
 			public void buttonClick(ClickEvent event) {
 				String ort = textField.getValue();
 				if (ort == null || ort.isEmpty()) {
-					Notification.show("Fehler", "Ort ist leer!", Type.ERROR_MESSAGE);
+					Notification.show("Fehler", "Ort ist leer!", Type.WARNING_MESSAGE);
 				} else {
+					anzahlSuchen++;
+
 					layout.addComponent(table);
+					table.setCaption(
+							MessageFormat.format("Treffer für {0} (Anzahl der Suchen: {1})", ort, anzahlSuchen));
 
 					List<Hotel> hotels = HotelSearch.getHotelSearch().getHotelByOrt(ort);
 					data.removeAllItems();
@@ -87,6 +92,26 @@ public class MyUI extends UI {
 					layout.addComponent(bucheButton);
 				}
 
+			}
+		});
+
+		table.addItemClickListener(new ItemClickListener() {
+
+			@Override
+			public void itemClick(ItemClickEvent event) {
+				BeanItem<Hotel> hotelItem = data.getItem(event.getItemId());
+				hotelSelektiert = hotelItem.getBean();
+			}
+		});
+
+		bucheButton.addClickListener(new ClickListener() {
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				if (hotelSelektiert == null) {
+					return;
+				}
+				// öffne Fenster zur Buchung
 			}
 		});
 
